6cc893413d37c8acce7bc79b25cd4a846d2b8620
roodriiguezzzz <adrianrodriguez2423@gmail.com>
2019-10-01 17:26:35 +0200

    Timestamp

--- smr2b.php
+++ smr2b.php
@@ -1,10 +1,11 @@
-<a href="http://192.168.0.101/smr2b_jesus/smr2b.php">jesus</a>
+<a href="http://192.168.0.177/smr2b_jesus/smr2b.php">jesus</a>
 adrian
 <a href="http://192.168.0.171/smr2b_cesar/smr2b.php">cesar</a>
 
 <?php
 $f=fopen('visitas.txt','a');
+fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
-fclose($f);
+fclose($f)
 ?>
