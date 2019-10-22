76a4257bca80dffb63748ca7e49740a61436760d
roodriiguezzzz <adrianrodriguez2423@gmail.com>
2019-10-22 16:45:19 +0200

    html.php

---
+++ html.php
@@ -0,0 +1,41 @@
+<html> 
+<head> 
+	<title>Adrian</title>
+</head>
+<body> 
+	<h1>Juan XXIII</h1>
+	Barrio donde vivo
+	<h2><u>Barrio transitado por POLICIA NACIONAL</u></h2>
+	EL LARGO
+	<h3>Tiene pista de futbol, un parque y unos bancos para estar de tranquis</h3>
+	Es un barrio muy tranquilo
+	</br>
+	<img src="j23.JPG"/>
+	<p> En las mismas, a través de un audio, se está difundiendo un robo a una mujer mayor utilizando para ello burundanga, algo que "no nos consta", han indicado fuentes policiales.</p>
+	<p>Desde este Cuerpo han aprovechado para hacer recomendaciones a la población y evitar ser víctima de este tipo de delitos, como desconfiar de personas que se hagan pasar por discapacitados psíquicos que se acerquen "con mucho dinero para regalarnos" y que a continuación otras personas que aparentan ser un abogado o gestor ofrezcan hacer negocio con esa cantidad a cambio de alguna joya.</p>
+	<form>
+	<input/>
+	<input type="submit" value="Enviar"/>
+    </form>
+	<body>
+  <font color="#FF0000";>
+  ESPAÑA ROJA
+  </font>
+	<table border>
+	<tr><td>Autonomía</td><td>Provincia</td></tr>
+	<tr><td>Galicia</td><td>Lugo</td></tr>
+	<tr><td>Galicia</td><td>La Coruña</td></tr>
+	<tr><td>Asturias</td><td>Asturias</td></tr>
+	<tr><td>Cantabria</td><td>Cantabria</td></tr>
+	<tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+	<tr><td>Pais Vasco</td><td>Guipuzcoa</td></tr>
+	</table>
+	<font color="#00FFFF">
+	<FONT SIZE=7>DAVID PONME UN 10</FONT>
+	</font>
+	</body>
+</body>
+</html>
+
+
+	
