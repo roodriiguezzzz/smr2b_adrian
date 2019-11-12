853551cdb293f0a9280b86670c8dac936976dbe1
roodriiguezzzz <adrianrodriguez2423@gmail.com>
2019-11-12 16:51:38 +0100

    catalogo.php

---
+++ catalogo.php
@@ -0,0 +1,41 @@
+
+<html>
+<head>
+<style>
+div {
+  border: 1px solid black;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  margin-right: 150px;
+  margin-left: 80px;
+  background-color: lightblue;
+}
+</style>
+</head>
+<body>
+
+</body>
+<html>
+	<head>
+		<style>
+			a{
+				display:block;
+				margin:10px;
+				border-style: solid;
+				border-width: thin;
+				border-radius: 5px;
+			}
+		</style>
+	<head>
+	<body>
+	    <div class="header">
+		<div><h1><H1>LISTA DE PERFILES DE GITUB</H1></h1><div>
+	<a href="https://github.com/dblancoformacion" target="_blank">Profesor David</a>
+	<a href="https://github.com/cesarc27" target="_blank">Cesar</a>
+	<a href="https://github.com/riki1" target="_blank">Ricardo</a>
+	<a href="https://github.com/villejobsv10" target="_blank">Rodrigo</a>
+	<a href="https://github.com/jesuscrespo" target="_blank">Jesús</a>
+	<a href="https://github.com/marioperezrodriguez" target="_blank">Mario</a>
+	<a href="https://github.com/mipadre" target="_blank">Mi padre</a>
+	</body>	
+</html>	
